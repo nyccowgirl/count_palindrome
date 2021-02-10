ee77d944805dbe17bbe1914c9db60a88c54ebedf
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-02-10 12:22:48 -0600

    refined code for case insensitivity

--- count_palindrome.py
+++ count_palindrome.py
@@ -1,9 +1,19 @@
+# Option 1:
 
-# print sum(1 for w in open('english.txt').read().splitlines() if w == w[::-1])  
+# To test locally:
 
-# print sum(1 for w in open('/users/abrick/resources/english').read().splitlines() if w == w[::-1])
+# print sum(1 for word in open('english.txt').read().splitlines() if word == word.lower()[::-1])  
 
-# print len(filter(lambda w: w == w[::-1], open('english.txt').read().splitlines()))
+# To test in hills:
 
-print len(filter(lambda w: w == w[::-1], open('/users/abrick/resources/english').read().splitlines()))
+# print sum(1 for word in open('/users/abrick/resources/english').read().splitlines() if word == word.lower()[::-1])
 
+# Option 2:
+
+# To test locally:
+
+# print len(filter(lambda word: word == word.lower()[::-1], open('english.txt').read().splitlines()))
+
+# To test in hills:
+
+print len(filter(lambda word: word == word.lower()[::-1], open('/users/abrick/resources/english').read().splitlines()))
