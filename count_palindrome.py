a940cd3d02e18ae64b7b1f0d2cdd437c0ec49fb6
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-02-11 02:16:09 -0600

    modified for Python 3

--- count_palindrome.py
+++ count_palindrome.py
@@ -2,18 +2,18 @@
 
 # To test locally:
 
-# print sum(1 for word in open('english.txt').read().splitlines() if word == word.lower()[::-1])  
+# print(sum(1 for word in open('english.txt').read().splitlines() if word == word.lower()[::-1]))  
 
 # To test in hills:
 
-# print sum(1 for word in open('/users/abrick/resources/english').read().splitlines() if word == word.lower()[::-1])
+# print(sum(1 for word in open('/users/abrick/resources/english').read().splitlines() if word == word.lower()[::-1]))
 
 # Option 2:
 
 # To test locally:
 
-# print len(filter(lambda word: word == word.lower()[::-1], open('english.txt').read().splitlines()))
+# print(sum(1 for _ in filter(lambda word: word == word.lower()[::-1], open('english.txt').read().splitlines())))
 
 # To test in hills:
 
-print len(filter(lambda word: word == word.lower()[::-1], open('/users/abrick/resources/english').read().splitlines()))
+print(sum(1 for _ in filter(lambda word: word == word.lower()[::-1], open('/users/abrick/resources/english').read().splitlines())))
